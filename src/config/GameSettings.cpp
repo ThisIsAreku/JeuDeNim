5b86aef7eb005af63048ba086915e612ba28224b
Alexandre Boucey <alexandre.boucey@alumni.univ-avignon.fr>
2014-04-25 14:08:35 +0200

    eval

--- src/config/GameSettings.cpp
+++ src/config/GameSettings.cpp
@@ -18,7 +18,7 @@ GameSettings::GameSettings()
     for(int i = 0; i < 4; i++)
         playerTypes[i] = 0;
     for(int i = 0; i < 4; i++)
-        aiLevel[i] = 1;
+        aiLevel[i] = 2;
 }
 
 GameSettings *GameSettings::setNumPlayers(int numPlayers)
